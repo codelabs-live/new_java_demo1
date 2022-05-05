be381d59a986adf642b23505486cc31180c08c30
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-05 14:21:46 +0530

    Done

--- src/db/MySqlConnection.java
+++ src/db/MySqlConnection.java
@@ -11,10 +11,11 @@ public class MySqlConnection {
             System.out.println("Done");
             PreparedStatement preparedStatement = connection.prepareStatement("select * from user");
 //            Statement statement = connection.createStatement();
-//            ResultSet rs = statement.executeQuery("select * from user");
-//            System.out.println(rs.getString(1));
-          preparedStatement.execute();
-            System.out.println(preparedStatement.getFetchSize());
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            System.out.println(rs.getString(1));
+//          preparedStatement.execute();
+//            System.out.println(preparedStatement.getFetchSize());
 
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e);
