0c82e24c416558b04a9cd36f66ffbbd4818f14ca
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-05 11:38:31 +0530

    Done

--- src/exception/ExceptionExample.java
+++ src/exception/ExceptionExample.java
@@ -4,13 +4,14 @@ public class ExceptionExample {
     public static void main(String[] args) {
 
 //        int a[] = {1,2,3,4,5,6};
-
+int a = Integer.parseInt("1s");
         try {
 //            System.out.println(a[6]);
-            System.out.println(3/0);
+//            System.out.println(3/0);
+            System.out.println(a);
         }catch (Exception e){
 //            System.out.println(a[5]);
-            System.out.println(e);
+//            System.out.println(e);
         }
 
     }
