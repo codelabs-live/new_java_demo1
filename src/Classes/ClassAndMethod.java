8aef8775347aa40d5687ed73e135560bfab6dc7f
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-07-13 08:33:24 +0530

    Done

--- src/Classes/ClassAndMethod.java
+++ src/Classes/ClassAndMethod.java
@@ -1,18 +1,95 @@
 package Classes;
+//
+//public class ClassAndMethod {
+//String name ;
+//    ClassAndMethod(String name){
+////        System.out.println(name);
+//        this.name =name;
+//    }
+//void display(){
+////        String nm;
+//    System.out.println(name);
+//}
+//    public static void main(String[] args) {
+//        ClassAndMethod obj = new ClassAndMethod("Kamlesh");
+//        obj.display();
+////        new ClassAndMethod("Kamlesh");
+//    }
+//}
+
+
+import java.util.Scanner;
+
+//public class ClassAndMethod {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String name ;
+//        System.out.println("Enter your name");
+//        name = scanner.next();
+////        System.out.println(name);
+//        if(name.equals("Kamlesh")){
+//            System.out.println(name);
+//        }else{
+//            System.out.println("Error");
+//        }
+//    }
+//}
+
+
+//public  class ClassAndMethod {
+//    public static void main(String[] args) {
+//        int a =1;
+//        int b =2;
+//        int c = a++;
+//        int d = ++b;
+//        System.out.println(c);
+//        System.out.println(d);
+//    }
+//}
+
+
+
+//public class ClassAndMethod {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int a,b;
+//        System.out.println("Enter the value of a ");
+//        a = scanner.nextInt();
+//        System.out.println("Enter the value of b");
+//        b = scanner.nextInt();
+//        if(a==3){
+//
+//            if(b==4){
+//                System.out.println(b);
+//            }else {
+//                System.out.println("Error");
+//                if(b==5){
+//
+//                }else {
+//
+//                }
+//            }
+//
+//
+//
+//            System.out.println(a);
+//        }else if(a==4) {
+//            System.out.println(a);
+//        }else {
+//            System.out.println("Error");
+//        }
+//    }
+//}
+
+
+
 
 public class ClassAndMethod {
-String name ;
-    ClassAndMethod(String name){
-//        System.out.println(name);
-        this.name =name;
-    }
-void display(){
-//        String nm;
-    System.out.println(name);
-}
     public static void main(String[] args) {
-        ClassAndMethod obj = new ClassAndMethod("Kamlesh");
-        obj.display();
-//        new ClassAndMethod("Kamlesh");
+        System.out.print("kgjsksjwts");
+        System.out.print("Demo");
+        System.out.print("Example");
+        System.out.print("Demo");
     }
 }
---
+++ src/NewProgram.java
@@ -0,0 +1,18 @@
+public class NewProgram {
+    public static void main(String[] args) {
+//        System.out.println("Example");
+ /*
+
+
+
+        System.out.println("Demo");
+  */
+
+
+//        System.out.print("Demo");
+//        System.out.print("Demo");
+
+
+
+    }
+}
